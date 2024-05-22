ffd73dbb2ac669792d0706f11f1cb1d4329a2ac6
edamame-maru <119129834+edamame-maru@users.noreply.github.com>
2024-05-22 18:28:22 +0100

    added broken euler2

--- euler2/src/main.rs
+++ euler2/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
+    let limit = 4000000;
+    let mut sum = 0;
 
+    let a = 1;
+    let b = 1;
+
+    while b < limit {
+        if b % 2 == 0 {
+            sum += b;
+        }
+    }
+
+    let mut c = a + b;
+    a = b;
+    b = c;
+
+    println!("{sum}");
 }
